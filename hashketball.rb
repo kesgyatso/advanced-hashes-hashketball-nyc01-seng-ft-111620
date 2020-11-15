08cd0a62859b8243a18efa07f53423273b5fe471
Kelsang Gyatso <kelsanggyatso@gmail.com>
2020-11-15 00:48:54 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,5 @@
-# Write your code below game_hash
+require 'pry'
+
 def game_hash
   {
     home: {
@@ -126,4 +127,21 @@ def game_hash
   }
 end
 
-# Write code here
+
+
+def num_points_scored (player_name)
+  game_hash
+  binding.pry
+  points_scored = []
+  
+  game_hash.each do
+    game_hash[:home]
+    
+    
+  }
+  end
+
+points_scored
+end
+
+
